c763a540ebaa2e2b646a2b34b5dd809c30c3493e
MrRaptor23 <123752881+MrRaptor23@users.noreply.github.com>
2023-05-28 09:42:52 +0300

    Add files via upload

--- Task36.py
+++ Task36.py
@@ -1,19 +1,22 @@
-def change_marks(marks: list[int]) -> list[int]:
-    """Рекурсивная замена макисмальных оценок минимальными"""
+'''Задача 36: Напишите функцию print_operation_table(operation, num_rows=6, num_columns=6),
+которая принимает в качестве аргумента функцию, вычисляющую элемент по номеру строки и
+столбца. Аргументы num_rows и num_columns указывают число строк и столбцов таблицы,
+которые должны быть распечатаны. Нумерация строк и столбцов идет с единицы (подумайте,
+почему не с нуля). Примечание: бинарной операцией называется любая операция, у которой
+ровно два аргумента, как, например, у операции умножения
+Ввод: print_operation_table(lambda x, y: x * y)
+Вывод:
+ 1 2 3 4 5 6
+ 2 4 6 8 10 12
+ 3 6 9 12 15 18
+ 4 8 12 16 20 24
+ 5 10 15 20 25 30
+ 6 12 18 24 30 36 '''
 
-    max_mark = max(marks)
-    min_mark = min(marks)
-    marks[marks.index(max_mark)] = min_mark
-
-    if max_mark not in marks:
-        return marks
-    return change_marks(marks)
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    a = [[operation(i, j) for j in range(1, num_columns + 1)] for i in range(1, num_rows + 1)]
+    for i in a:
+        print(*[f"{x:>3}" for x in i])
 
 
-print(change_marks([1, 2, 3, 1, 1, 3, 4, 4, 5, 4]))
-
-a = [1, 2, 3, 1, 1, 3, 4, 5, 5, 5]
-print(a.index(5))
-
-a[a.index(5)] = 1000
-print(a)
+print_operation_table(lambda x, y: x * y)
