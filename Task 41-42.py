ecddd7362335272f47e0e5fb521f1875ee3b82a4
MrRaptor23 <123752881+MrRaptor23@users.noreply.github.com>
2023-06-04 15:42:51 +0300

    Add files via upload

---
+++ Task 41-42.py
@@ -0,0 +1,15 @@
+Задание
+# Задача 40: Работать с файлом california_housing_train.csv, который находится в папке
+# sample_data. Определить среднюю стоимость дома, где кол-во людей от 0 до 500 (population)
+# ​
+# Вариант 2: ​
+df[df['population']<501]['median_house_value'].mean()
+206799.95140186916
+
+# Задача 42: Узнать какая максимальная households в зоне минимального значения population
+
+# Решение:
+# ​
+df[df['population']==df['population'].min()]['households'].max()
+# 4.0
+​
